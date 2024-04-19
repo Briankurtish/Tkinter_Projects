700917625e9d4ac17f612463ef0daa0b11a152e0
cipher <asaforbrian18@gmail.com>
2024-04-19 18:18:19 +0300

    commit

---
+++ demo.py
@@ -0,0 +1,28 @@
+# hours_worked = int(input("Enter Hours worked: "))
+# hourly_wage = int(input("Enter the Hourly Wage: "))
+
+# salary =  (hours_worked * hourly_wage)
+
+# if salary >= 5000:
+#     print("You are a Senior Employee")
+# elif salary <= 2000 and salary > 5000:
+#     print("You are a Mid Level Employee")
+# else:
+#     print("You are a Beginner at the company")
+    
+    
+    
+# def add(num1, num2):
+#     return num1 + num2
+
+# result = add(3,4)
+
+# print(result)
+
+# for i in range(1, 10):
+#     print(i)
+#     i+=1
+i=0
+while i <= 10:
+    print(i)
+    i += 1
